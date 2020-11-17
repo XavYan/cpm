5d09595364ed9a8e6f7dbed078fd7905830e7c5f
xavyan <xavieryanquin@gmail.com>
2020-11-17 18:15:20 +0000

    Modified 'add' command
    Now it works to create a simple non-dependency class, and import it to Makefile for compiling.

--- commands/add.py
+++ commands/add.py
@@ -1,8 +1,9 @@
 from .interface import CommandInterface
+from makefile import Makefile
 
 from decouple import config
 from os import listdir
-from os.path import join
+from os.path import join, exists
 from shutil import copytree
 
 
@@ -31,6 +32,32 @@ class CommandAdd(CommandInterface):
         return "Module added to " + config('DIR_PATH') + " successfully"
 
     def execute(self, module):
+        try:
+            header_filename = 'include/{}.h'.format(module)
+            source_filename = 'src/{}.cc'.format(module)
+
+            if exists(header_filename) or exists(source_filename):
+                raise FileExistsError
+
+            with open(header_filename, 'w') as file:
+                file.write('#pragma once\n\n')
+                file.write('class {} {{\n'.format(module))
+                file.write('public:\n')
+                file.write('\t{}();\n'.format(module))
+                file.write('};\n')
+
+            with open(source_filename, 'w') as file:
+                file.write('#include <{}.h>\n\n'.format(module))
+                file.write('{module}::{module}() {{}}\n'.format(module=module))
+
+            Makefile.add_action(module)
+
+        except FileNotFoundError:
+            print("Cannot execute add:", "file not found")
+        except FileExistsError:
+            print("Cannot execute add:", module, "already exists in this project")
+
+    def old_execute(self, module):
         try:
             files = listdir(module)
             for file in files:
