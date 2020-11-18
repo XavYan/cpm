fcbef84b39a529cb948c03a45aebdbb226b48a62
xavyan <xavieryanquin@gmail.com>
2020-11-18 20:24:05 +0000

    Refactor of commands system

--- cpm.py
+++ cpm.py
@@ -7,12 +7,28 @@ from os.path import basename
 import commands
 import subprocess
 
+
+def add_option(pars, long_option, short_option="", help_msg="", arg_name="", act=""):
+    # print("action:", act, "metavar:", arg_name)
+    if short_option:
+        if act == "store_true" or act == "store_false":
+            pars.add_argument(long_option, short_option, help=help_msg, action=act)
+        else:
+            pars.add_argument(long_option, short_option, help=help_msg, metavar=arg_name, action=act)
+    else:
+        if act == "store_true" or act == "store_false":
+            pars.add_argument(long_option, help=help_msg, action=act)
+        else:
+            pars.add_argument(long_option, help=help_msg, metavar=arg_name, action=act)
+
+
 options = [
     'add',
     'tadd',
     'install',
     'init',
-    'remove'
+    'remove',
+    'run'
 ]
 
 cms = {}
@@ -21,29 +37,24 @@ parser = argp.ArgumentParser(prog="cpm")
 
 for option in options:
     cms[option] = commands.initialize_command(option)
-    if cms[option].short_option():
-        parser.add_argument(cms[option].short_option(), cms[option].long_option(), help=cms[option].help(),
-                            metavar=cms[option].argument_name())
-    else:
-        parser.add_argument(cms[option].long_option(), help=cms[option].help(), metavar=cms[option].argument_name())
+    argname = ""
+    if cms[option].argument_name():
+        argname = cms[option].argument_name()
+    add_option(parser, cms[option].long_option(), short_option=cms[option].short_option(), help_msg=cms[option].help(),
+               arg_name=argname, act=cms[option].action())
 
-parser.add_argument('-g', '--global', action='store_true',
+parser.add_argument('--global', '-g', action='store_true',
                     help="Use global methods instead", dest="gl")
-parser.add_argument('-b', '--build', action='store_true',
-                    help="Compile and execute program. Use it in root directory")
-parser.add_argument('-r', '--run', action='store_true',
-                    help="Compile and execute program."
-                         "It deletes the executable after executing. Use it in root directory")
+add_option(parser, '--build', short_option='-b', help_msg="Compile and execute program. Use it in root directory",
+           act="store_true")
 
 args = parser.parse_args()
 
-if args.build or args.run:
+if args.build:
     subprocess.call('make')
     print('---------------------------------------')
     executable = './{}'.format(basename(getcwd()))
     subprocess.call(executable)
-    if args.run:
-        remove(executable)
     exit()
 
 if not args.gl and len(argv) > 3:
