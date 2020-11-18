39888a9806d5a2f94202e4dd79946d75852f3097
xavyan <xavieryanquin@gmail.com>
2020-11-18 00:43:03 +0000

    Fixed problems with INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,14 +7,24 @@ fi
 mkdir $USER_HOME/.cpm_core
 chmod 777 $USER_HOME/.cpm_core
 
-file_list="commands cpm.py makefile.py .env"
+file_list="commands cpm.py makefile.py"
 
 for file in $file_list; do
   cp -R $file $USER_HOME/.cpm_core/
 done
 
+while IFS= read -r line
+do
+  if [[ $line == *DIR_PATH* ]]; then
+    echo "DIR_PATH = \"$USER_HOME/cpm_modules\"" >> $USER_HOME/.cpm_core/.env
+  else
+    echo $line >> $USER_HOME/.cpm_core/.env
+  fi
+done < .env
+
 if [ ! -e $USER_HOME/cpm_modules ]; then
   mkdir $USER_HOME/cpm_modules
+  chmod 777 $USER_HOME/cpm_modules
 fi
 
 pip3 install python-decouple
