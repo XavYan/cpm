61e7edd0bb7aa4e3835d2394f9801f2f02ab71d8
xavyan <xavieryanquin@gmail.com>
2020-11-18 18:04:53 +0000

    Created class CommandArgInterface and CommandBoolInterface to better command manipulation

---
+++ commands/command_arg_interface.py
@@ -0,0 +1,30 @@
+from .command_interface import CommandInterface
+
+
+class CommandArgInterface(CommandInterface):
+    def __str__(self):
+        return "argument_command"
+
+    def action(self):
+        return "store"
+
+    def argument_name(self):
+        return "argument"
+
+    def help(self):
+        raise NotImplementedError
+
+    def short_option(self):
+        raise NotImplementedError
+
+    def long_option(self):
+        raise NotImplementedError
+
+    def success_text(self):
+        raise NotImplementedError
+
+    def fail_text(self):
+        raise NotImplementedError
+
+    def execute(self, arg):
+        raise NotImplementedError
---
+++ commands/command_bool_interface.py
@@ -0,0 +1,27 @@
+from .command_interface import CommandInterface
+
+
+class CommandBoolInterface(CommandInterface):
+    def __str__(self):
+        return "command"
+
+    def action(self):
+        return "store_true"
+
+    def help(self):
+        raise NotImplementedError
+
+    def short_option(self):
+        raise NotImplementedError
+
+    def long_option(self):
+        raise NotImplementedError
+
+    def success_text(self):
+        raise NotImplementedError
+
+    def fail_text(self):
+        raise NotImplementedError
+
+    def execute(self):
+        raise NotImplementedError
