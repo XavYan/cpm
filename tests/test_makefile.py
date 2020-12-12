cca7a96e0b6e0abd642fd459ae7d550b4918bffa
xavyan <xavieryanquin@gmail.com>
2020-12-12 21:57:40 +0000

    Created tests for Makefile class

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -6,22 +6,89 @@ from tests.writefake import WriterFake
 
 class TestMakefile(TestCase):
     def setUp(self):
-        self.module = "matrix"
+        self.package = "matrix"
+        self.action = "vector"
+
+        self.writerfake = WriterFake()
+        self.makefile = Makefile(self.writerfake)
 
     def test_generate(self):
-        self.fail()
+        expected_lines = [
+            "CC=g++",
+            "CFLAGS=-g -std=c++17 -Wall -Wextra -Iinclude -Iutils",
+            "\n",
+            "all: src\\main.cc",
+            "\t$(CC) $(CFLAGS) -o {package} src\\main.cc".format(package=self.package),
+            "\n"
+        ]
+
+        self.makefile.generate(self.package)
+
+        final_lines = self.writerfake.get_lines()
+        self.assertEqual(expected_lines, final_lines)
 
     def test_add_variable(self):
-        self.fail()
+        expected_lines = ["CC=g++"]
+
+        self.makefile.add_variable('Makefile', 'CC', 'g++')
+
+        final_lines = self.writerfake.get_lines()
+        self.assertEqual(expected_lines, final_lines)
 
     def test_add_action(self):
-        self.fail()
+        expected_lines = [
+            "all: src\\main.cc build\\{action}.o".format(action=self.action),
+            "\t$(CC) $(CFLAGS) -o {package} src\\main.cc build\\{action}.o".format(package=self.package, action=self.action),
+            "\n",
+            "build\\{action}.o: include\\{action}.h src\\{action}.cc".format(action=self.action),
+            "\t$(CC) $(CFLAGS) -c -o build\\{action}.o src\\{action}.cc".format(action=self.action)
+        ]
+
+        self.makefile.add_base_all_action('Makefile', self.package)
+        self.makefile.add_action(self.action)
+
+        written_lines = self.writerfake.get_lines()
+        self.assertEqual(expected_lines, written_lines)
 
     def test_delete_action(self):
-        self.fail()
+        initial_lines = [
+            "all: src\\main.cc build\\{action}.o".format(action=self.action),
+            "\t$(CC) $(CFLAGS) -o {package} src\\main.cc build\\{action}.o".format(package=self.package, action=self.action),
+            "\n",
+            "build\\{action}.o: include\\{action}.h src\\{action}.cc".format(action=self.action),
+            "\t$(CC) $(CFLAGS) -c -o build\\{action}.o src\\{action}.cc".format(action=self.action)
+        ]
+        expected_lines = [
+            "all: src\\main.cc",
+            "\t$(CC) $(CFLAGS) -o {package} src\\main.cc".format(package=self.package),
+            "\n"
+        ]
+
+        self.writerfake.set_lines(initial_lines)
+
+        self.makefile.delete_action(self.action)
+
+        final_lines = self.writerfake.get_lines()
+        self.assertEqual(expected_lines, final_lines)
 
     def test_update_all_with_util(self):
-        self.fail()
+        new_util = "utils/{action}\\{action}.o".format(action=self.action)
+
+        initial_lines = [
+            "all: src\\main.cc",
+            "\t$(CC) $(CFLAGS) -o {package} src\\main.cc".format(package=self.package),
+            "\n"
+        ]
+        expected_lines = [
+            "all: src\\main.cc {util}".format(util=new_util),
+            "\t$(CC) $(CFLAGS) -o matrix src\\main.cc {util}".format(util=new_util),
+            "\n"
+        ]
+
+        self.writerfake.set_lines(initial_lines)
+        self.makefile.update_all_with_util('Makefile', self.action)
+
+        self.assertEqual(expected_lines, self.writerfake.get_lines())
 
     def test_update_all_with_module(self):
         lines = [
@@ -30,35 +97,32 @@ class TestMakefile(TestCase):
             "\n"
         ]
 
-        writerfake = WriterFake()
-        makefile = Makefile(writerfake)
-
-        writerfake.set_lines_to_read(lines)
+        self.writerfake.set_lines(lines)
 
         lines[0] += " build\\matrix.o"
         lines[1] += " build\\matrix.o"
 
-        makefile.update_all_with_module('Makefile', 'matrix')
+        self.makefile.update_all_with_module('Makefile', 'matrix')
 
-        self.assertEqual(writerfake.get_written_lines(), lines)
+        self.assertEqual(self.writerfake.get_lines(), lines)
 
-    def test_add_base_all_action(self):
-
-        lines = [
+    def test_add_base_all_action_with_separator(self):
+        expected_lines = [
             "all: src\\main.cc",
             "\t$(CC) $(CFLAGS) -o matrix src\\main.cc",
             "\n"
         ]
 
-        writerfake = WriterFake()
-        makefile = Makefile(writerfake)
-        makefile.add_base_all_action('Makefile', 'matrix')
+        self.makefile.add_base_all_action('Makefile', 'matrix')
 
-        self.assertEqual(writerfake.get_written_lines(), lines)
+        self.assertEqual(self.writerfake.get_lines(), expected_lines)
 
-        lines.pop()
-        writerfake.clean_buffer()
+    def test_add_base_all_action_without_separator(self):
+        expected_lines = [
+            "all: src\\main.cc",
+            "\t$(CC) $(CFLAGS) -o matrix src\\main.cc"
+        ]
 
-        makefile.add_base_all_action('Makefile', 'matrix', separator=False)
+        self.makefile.add_base_all_action('Makefile', 'matrix', separator=False)
 
-        self.assertEqual(writerfake.get_written_lines(), lines)
+        self.assertEqual(expected_lines, self.writerfake.get_lines())
--- tests/writefake.py
+++ tests/writefake.py
@@ -1,28 +1,27 @@
 from copy import deepcopy
 
+
 class WriterFake:
     def __init__(self):
-        self.last_read_lines = []
-        self.last_wrote_lines = []
+        self.lines = []
 
-    def set_lines_to_read(self, lines):
-        self.last_read_lines = deepcopy(lines)
+    def set_lines(self, lines):
+        self.lines = deepcopy(lines)
 
     def clean_buffer(self):
-        self.last_read_lines = []
-        self.last_wrote_lines = []
+        self.lines = []
 
-    def get_written_lines(self):
-        return self.last_wrote_lines
+    def get_lines(self):
+        return self.lines
 
     def read_lines(self, filename):
-        return self.last_read_lines
+        return self.lines
 
     def write_lines(self, lines, filename):
-        self.last_wrote_lines = lines
+        self.lines = lines
 
     def append_lines(self, lines, filename):
-        self.last_wrote_lines.extend(lines)
+        self.lines.extend(lines)
 
     def append_empty_line(self, filename):
-        self.last_wrote_lines.append('\n')
+        self.lines.append('\n')
