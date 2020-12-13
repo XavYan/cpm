c7dae5c09f711e283d7f104812d41e08364c4204
xavyan <xavieryanquin@gmail.com>
2020-12-13 20:51:40 +0000

    Created tests for tadd command
    Issue #7

---
+++ tests/test_command_tadd.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+
+from commands import CommandAdd
+from makefile import Makefile
+from tests.writefake import WriterFake
+
+
+class TestCommandTemplateAdd(TestCase):
+    def setUp(self):
+        self.module = "vector"
+        self.writerfake = WriterFake()
+        self.makefile = Makefile(self.writerfake)
+        self.cmd_tadd = CommandAdd(self.makefile, self.writerfake, template=True)
+
+    def test_str_is_tadd(self):
+        self.assertEqual(self.cmd_tadd.__str__(), "tadd")
+
+    def test_short_option(self):
+        self.assertEqual(self.cmd_tadd.short_option(), "-t")
+
+    def test_long_option(self):
+        self.assertEqual(self.cmd_tadd.long_option(), "--tadd")
+
+    def test_success_text(self):
+        self.assertEqual(type(self.cmd_tadd.help()), str)
+
+    def test__add_header_file(self):
+        expected_lines = [
+            '#pragma once',
+            '\n',
+            'template<class T>',
+            'class {module} {{'.format(module=self.module),
+            'public:',
+            '\t{module}();'.format(module=self.module),
+            '};',
+            '\n',
+            'template<class T>',
+            '{module}<T>::{module}() {{}}'.format(module=self.module)
+        ]
+
+        self.cmd_tadd._add_header_file(self.module, template=True)
+
+        self.assertEqual(expected_lines, self.writerfake.get_lines())
